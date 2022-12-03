6d90953ca35e4220806b570e0656fb848f7140e1
01110101 <42702073+alisherweb3@users.noreply.github.com>
2022-12-03 18:59:07 +0500

    Create math_floor.py

---
+++ freeCode/math_floor.py
@@ -0,0 +1,8 @@
+import math
+
+print(math.floor(0.6)) # 0
+print(math.floor(1.4)) # 1
+print(math.floor(5.3)) # 5
+print(math.floor(-5.3)) # -6
+print(math.floor(22.6)) # 22
+print(math.floor(10.0)) # 10
